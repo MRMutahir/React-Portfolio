433a09e70bed85033d27bca080ddd035532f3ece
MRMutahir <mutahirkareem820@gmail>
2023-09-05 09:33:06 -0700

    recent project done

--- src/Components/RecentProject/RecentProject.jsx
+++ src/Components/RecentProject/RecentProject.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "../../Components/RecentProject/RecentProject.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import Sidebar from "../../img/sidebar.png";
+import Ecommerce from "../../img/ecommerce.png";
+import HOC from "../../img/hoc.png";
+import MusicApp from "../../img/musicapp.png";
 
 function RecentProject() {
-  return <div className="r-Project">RecentProject</div>;
+  return (
+    <div className="r-Project">
+      <span>Recent Project</span>
+      <span>Portfolio</span>
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={3}
+        grabCursor={true}
+        className="Portfolio-slider"
+      >
+        <SwiperSlide>
+          <img src={Sidebar} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Ecommerce} alt="" />
+        </SwiperSlide>{" "}
+        <SwiperSlide>
+          <img src={HOC} alt="" />
+        </SwiperSlide>{" "}
+        <SwiperSlide>
+          <img src={MusicApp} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
 
 export default RecentProject;
